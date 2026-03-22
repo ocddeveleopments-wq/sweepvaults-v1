55168685415d8cee1823634a709488030a9e4953
OCDDE <ocddeveleopments@gmail.com>
2026-03-22 18:50:26 -0400

    fix prisma client with ts-ignore

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,11 @@
-import { PrismaClient } from "@prisma/client/scripts/default-index.js"
+// @ts-ignore
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  prisma: any | undefined
 }
 
-export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
