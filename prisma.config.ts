86d15ca8b79f966b1cd8d01fa3cf96bba961143f
OCDDE <ocddeveleopments@gmail.com>
2026-03-22 17:03:12 -0400

    fix prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,9 +2,8 @@ import path from "node:path"
 import { defineConfig } from "prisma/config"
 
 export default defineConfig({
-  earlyAccess: true,
   schema: path.join(__dirname, "prisma/schema.prisma"),
   datasource: {
-    url: "postgresql://postgres.fypjyivdghkqposuunqd:Sweep2026!!!@aws-1-us-east-1.pooler.supabase.com:5432/postgres",
+    url: process.env.DIRECT_URL!,
   },
 })
